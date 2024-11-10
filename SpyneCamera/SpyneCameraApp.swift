f93bbfe9dd83542ae1cb1becc7121518507bc45c
Aryan Sharma <agsharma98@gmail.com>
2024-11-09 23:31:04 -0800

    Initial Commit

---
+++ SpyneCamera/SpyneCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpyneCameraApp.swift
+//  SpyneCamera
+//
+//  Created by Aryan Sharma on 09/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SpyneCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
